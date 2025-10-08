da9b1b707a156eb36c2f4f7d168b361302fb5def
Ed Calvert <72658447+EdwardCalvert@users.noreply.github.com>
2025-10-08 16:52:48 +0100

    feat: perfomance enhancements

--- Edward_AberWay.py
+++ Edward_AberWay.py
@@ -13,10 +13,10 @@ update(None, screen, bg, lineList, nodeList, None, None, None, None, 0)
 
 
 # --- SET THESE VALUES TO AN EXAMPLE ---
-startPos = 0
-listOfNodesToPass = [34, 27, 17]
-length = 758.97
-error = 0.06
+startPos = 47
+listOfNodesToPass = [34, 19, 0, 12]
+length = 2044.79
+error = 0.14
 
 
 def path_update():
@@ -24,51 +24,31 @@ def path_update():
     start = time.time_ns()  # for timing your algorithm
     # ---------- ---------- YOUR CODE GOES HERE ---------- ----------
 
-    def calculate_distance_between_nodeList_elements(from_node, to_node):
-
-        pass
-
+    possible_paths = []
     def find_paths(start, path=[], current_length=0):
         path = path + [start]
 
         if current_length >= length - error and current_length <= length + error:
-            print("=====>Found path:", path)
+            possible_paths.append(path)
+            return
 
         if current_length > length + error:
-            # print(f"Path too long: {current_length}")
             return
 
         neighbors = nodeList[start][3]
-        # print(neighbors)
         for neighbor in neighbors:
-
-            totalDist = 0
-            for line in lineList:
-                l = line[4]
-                
-                if l == [start, neighbor]:
-                    # add the distance that has been traveled
-                    if line[1] != None:
-                        totalDist += math.dist(line[0], line[1])
-                    else:
-                        totalDist += math.dist(line[0][-1], line[0][0])
-                    
-                l.reverse()
-                if l == [start, neighbor]:
-
-                    # add the distance that has been traveled
-                    if line[1] != None:
-                        totalDist += math.dist(line[0], line[1])
-                    else:
-                        totalDist += math.dist(line[0][-1], line[0][0])
-
-
+            dist =  math.dist(nodeList[start][0], nodeList[neighbor][0])
             if neighbor not in path:  # avoid cycles, if required
-                find_paths(neighbor, path, current_length + totalDist)
+                find_paths(neighbor, path, current_length + dist)
 
-    find_paths(0)
+    find_paths(startPos)
 
-    ListOfNodeId = [0, 2, 10, 13, 11, 14, 15]
+    for path in possible_paths:
+        if set(listOfNodesToPass).issubset(path):
+            print(f"Valid: {path}")
+            ListOfNodeId = path
+            pass
+    #ListOfNodeId = [0, 2, 10, 13, 11, 14, 15]
     # ---------- ---------- ---------- ---------- ---------- ---------- ----------
     end = time.time_ns()
     update(ListOfNodeId, screen, bg, lineList, nodeList, startPos,
